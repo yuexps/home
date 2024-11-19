681c1fb8fef57f50cbebab83e46a66bfca335fa0
yuexps <69074603+yuexps@users.noreply.github.com>
2024-11-19 21:54:27 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,9 +16,7 @@
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
           &amp;&nbsp;Made&nbsp;by
-          <a :href="config.github" target="_blank">
             {{ config.author }}
-          </a>
         </span>
         <!-- 站点备案 -->
         <span>
